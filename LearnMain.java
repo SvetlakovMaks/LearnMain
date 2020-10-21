aaf782dbcd5c4fc4c8fb5d44e3af6018226735ef
Maksim Svetlakov <j-smy18@mail.ru>
2020-10-21 18:34:30 +0400

    praktika Git

---
+++ LearnMain.java
@@ -0,0 +1,7 @@
+public class LearnMain {
+    public static void main(String[] args) {
+        System.out.println("hello Java");
+        System.out.println(42);
+        System.out.println("\n\tJava " + 11);
+    }
+}
